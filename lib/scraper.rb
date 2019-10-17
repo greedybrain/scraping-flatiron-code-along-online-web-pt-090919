213e8d2429b80d780101f3bccc99a569db45fb23
Naya  Willis <greedybrain18@gmail.com>
2019-10-17 01:12:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
@@ -16,7 +17,46 @@ class Scraper
     end
   end
   
+  def get_page
+    html = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    html
+  end
+  
+  def get_courses
+    courses = get_page.css('article.post') # avoid the empty ones
+    courses
+    # binding.pry
+  end
+  
+  def make_courses
+    course_instances = get_courses.collect do |course|
+      new_inst = Course.new 
+      new_inst.title = course.css('h2').text 
+      new_inst.schedule = course.css('em.date').text
+      new_inst.description = course.css('p').text
+      # binding.pry
+    end
+  end
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
